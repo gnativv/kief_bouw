9a972c4e02f99207dcecad2806c4ea46dbd6f175
Valentyn Gnativ <valentyn.gnativ@student.uva.nl>
2024-07-04 14:01:27 +0200

    setup

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home.tsx';
+import About from './pages/About.tsx';
+import Services from './pages/Services.tsx';
+import Contact from './pages/Contact.tsx';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+export default App;
---
+++ src/pages/Services.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services: React.FC = () => {
+  return (
+    <div>
+      <h1>Our Services</h1>
+      <p>Discover the services we offer.</p>
+    </div>
+  );
+}
+
+export default Services;
